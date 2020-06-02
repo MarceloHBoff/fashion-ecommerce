87826b05a54591cf0faa2e95467c2396e177291d
MarceloHBoff <marceloboff20@gmail.com>
2020-06-01 22:34:14 -0300

    Update readme

--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-import data from '../../assets/data.json';
 import { IProduct } from '../../store/modules/product/types';
 import Card from '../Card';
 import Shimmer from '../Shimmer';
@@ -14,11 +13,9 @@ const ProductsList: React.FC = () => {
 
   useEffect(() => {
     setTimeout(() => {
-      setProducts(data.products);
-
-      // fetch('https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog')
-      //   .then(response => response.json())
-      //   .then(response => setProducts(response));
+      fetch('https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog')
+        .then(response => response.json())
+        .then(response => setProducts(response));
     }, 1);
   }, []);
 
